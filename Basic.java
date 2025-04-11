12a67569c4f460e5c4aed8b579a1a75a4f9901f8
shantiroy1 <shantiroy2015@gmail.com>
2025-04-11 17:10:20 -0400

    Add files via upload

---
+++ Basic.java
@@ -0,0 +1,15 @@
+package pack1;
+
+public class Basic {
+
+	public static void main(String[] args) {
+		int dob=1996;
+		System.out.println(" Hello everyone ");
+		System.out.println(" This is our Automation class");
+		System.out.println(" We learn Java ");
+		System.out.println(" My birth year is "+dob);
+		
+				
+	}
+
+}
